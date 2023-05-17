b4695a62744c3a50087fadc2a5d8f1593cd3dd58
parekhankit3 <94030822+parekhankit3@users.noreply.github.com>
2023-05-17 16:39:39 +0100

    rtu

--- providers.tf
+++ providers.tf
@@ -52,12 +52,6 @@ resource "aws_route_table" "terraformRT" {
     cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.igwterraform.id
   }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.igwterraform.id
-  }
-
   tags = {
     Name = "terraformRT"
   }
