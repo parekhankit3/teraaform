8d3eb945f24fbd46414834d5620373a2c8fbd6bc
parekhankit3 <94030822+parekhankit3@users.noreply.github.com>
2023-05-17 17:16:44 +0100

    un

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myec2" {
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
--- providers.tf
+++ providers.tf
@@ -88,3 +88,13 @@ resource "aws_security_group" "terraformSecGroup" {
     Name = "terraformSecGroup"
   }
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
