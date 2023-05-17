05f8327c30f74e9def5ad14dad1d0100418c2657
parekhankit3 <94030822+parekhankit3@users.noreply.github.com>
2023-05-17 16:56:11 +0100

    s

--- providers.tf
+++ providers.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "terraformRT" {
 }
 
 # create subnet endpoint associate
-resource "aws_vpc_endpoint_subnet_association" "sn_ec2" {
+resource "aws_vpc_endpoint_subnet_association" "myec2" {
   vpc_endpoint_id = aws_vpc_endpoint.terraformvpc.id
   subnet_id       = aws_subnet.terraformsubnet.id
 }
