aaf213ba6d4cccf946654e1dac64ed62eeee457a
parekhankit3 <94030822+parekhankit3@users.noreply.github.com>
2023-05-21 15:23:29 +0100

    x

--- providers.tf
+++ providers.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "myec2" {
   ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
   subnet_id     = "terraformsubnet"
-  vpc_id = "terraformvpc"
+  
  
 
   tags = {
